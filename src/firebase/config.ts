c50476b6b8009b7dd57f24296bd62a53fc63db50
Ebrima Jallow <jallowebrima7@gmail.com>
2025-10-19 18:27:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1074412180-52709",
+  "appId": "1:900776189547:web:15cb8d5aba7c51db319b2e",
+  "apiKey": "AIzaSyBduhNCnr_gvL6v6Xy3aI-04QMGhAK9nsk",
+  "authDomain": "studio-1074412180-52709.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "900776189547"
+};
