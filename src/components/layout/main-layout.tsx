93bd9bf97ed6ec8cc5fa0fecd47b54e7a4289063
Ebrima Jallow <jallowebrima7@gmail.com>
2025-10-20 11:57:47 +0000

    change the main-layout grid screen size from md to large.

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -3,14 +3,14 @@ import type { ReactNode } from 'react';
 export function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div className="container flex-1">
-      <div className="grid grid-cols-1 md:grid-cols-6 gap-8">
-        <aside className="hidden md:block md:col-span-1">
+      <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
+        <aside className="hidden lg:block lg:col-span-1">
           {/* Left sidebar content goes here */}
         </aside>
-        <main className="md:col-span-4">
+        <main className="lg:col-span-4">
           {children}
         </main>
-        <aside className="hidden md:block md:col-span-1">
+        <aside className="hidden lg:block lg:col-span-1">
           {/* Right sidebar content goes here */}
         </aside>
       </div>
