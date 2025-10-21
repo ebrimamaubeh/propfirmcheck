d0505709d5e4740e205205907304cf3066f2bed9
Ebrima Jallow <ebrimamaubeh7@gmail.com>
2025-10-21 19:50:50 +0200

    Update config.ts
    this is not suppose to show.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1074412180-52709",
-  "appId": "1:900776189547:web:15cb8d5aba7c51db319b2e",
-  "apiKey": "AIzaSyB1K4pyToiFcSBtWeeHs8GAr5QKy5uhNmc",
+  "appId": "",
+  "apiKey": "",
   "authDomain": "studio-1074412180-52709.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "900776189547"
